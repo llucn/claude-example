d13207597bbecdf4861029acca34981eb9bbe0dd
Lane Lu <161492667+llucn@users.noreply.github.com>
2026-04-09 13:30:59 +0800

    use env config (#6)

--- packages/mobile/src/app/auth/oidc-config.ts
+++ packages/mobile/src/app/auth/oidc-config.ts
@@ -1,8 +1,8 @@
 import { makeRedirectUri } from 'expo-auth-session';
 
-export const OIDC_AUTHORITY = 'https://auth.developbranch.cn/realms/bsr';
-export const OIDC_CLIENT_ID = 'bsr-mobile';
-export const OIDC_SCOPE = 'openid profile email';
+export const OIDC_AUTHORITY = process.env.EXPO_PUBLIC_OIDC_AUTHORITY;
+export const OIDC_CLIENT_ID = process.env.EXPO_PUBLIC_OIDC_CLIENT_ID;
+export const OIDC_SCOPE = process.env.EXPO_PUBLIC_OIDC_SCOPE;
 
 export const OIDC_REDIRECT_URI = makeRedirectUri({
   scheme: 'claude-example-mobile',
